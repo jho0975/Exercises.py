3b292a70b2e2101679f11d7176d9b84bc22fdf42
Moruri_P <106961144+jho0975@users.noreply.github.com>
2025-07-22 15:04:17 +0200

    Add files via upload

---
+++ exercises.py
@@ -0,0 +1,43 @@
+print("Palesa")
+print(32)
+
+name= "Palesa"
+country= "South Africa"
+favourite_colour= "Blue"
+
+print(f"My name is {name}, I live in {country}, and my favourite color is {favourite_colour}")
+
+x = 5
+y = "5"
+print(x + int(y))  
+
+print(f"Hello {name}")
+
+age= 32             #integer
+height= 1.65        #float
+likes_coding= True  #boolean
+
+name = "Palesa"
+favourite_number = 7
+is_student = True
+
+print (name)
+print (favourite_number)
+print (is_student)
+
+print((10+5)*2-3)
+
+a= 42
+b= "hello"
+c= 3.52
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+
+my_name= "Palesa"
+my_age= 32
+is_programmer= True
+
+print("About Me:")
+print(f"My name is {my_name}, I am {my_age} years old, and it is {is_programmer} that I am a programmer.")
